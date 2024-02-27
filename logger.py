a67eb22d337fdc84d430c7e1d94321ddc8bc7b00
AngieLuts <angiegk@yandex.ru>
2024-02-27 23:40:33 +0300

    added some defs

--- logger.py
+++ logger.py
@@ -1,5 +1,4 @@
 from data_create import name_data, surname_data, phone_data, address_data
- 
 
 
 def input_data():
@@ -8,44 +7,95 @@ def input_data():
     phone = phone_data()
     address = address_data()
     var = int(input(f"choose format: \n\n"
-    f" 1 variant: \n"
-    f"{name}\n {surname} \n {phone} \n {address}\n\n"
-    f"{name}; {surname}; {phone}; {address} \n"
-    f" choose, pls"))
+                    f" 1 variant: \n"
+                    f"{name}\n {surname} \n {phone} \n {address}\n\n"
+                    f"{name}; {surname}; {phone}; {address} \n"
+                    f" choose, pls"))
     while var != 1 and var != 2:
         print('incorrect')
         var = int(input('type in: '))
-        
+
     if var == 1:
         with open('data_ver1.csv', 'a', encoding='utf-8') as f:
             f.write(f"{name}\n{surname}\n{phone}\n{address}\n\n")
-            
+
     elif var == 2:
         with open('data_ver2.csv', 'a', encoding='utf-8') as f:
-            f.write (f"{name}; {surname}; {phone}; {address} \n")
-
+            f.write(f"{name}; {surname}; {phone}; {address} \n")
 
 
 def print_data():
-    print (' PRINTING OUT from the first file: \n')
+    print(' PRINTING OUT from the first file: \n')
     with open('data_ver1.csv', 'r', encoding='utf-8') as f:
         data_first = f. readlines()
-        data_first_list= []
-        j=0
+        data_first_list = []
+        j = 0
         for i in range(len(data_first)):
-            if  data_first[i] == '\n' or data_first[i]== len(data_first) - 1:
-                data_first_list.append('' . join(data_first[j : i + 1]))
-                j=i
+            if data_first[i] == '\n' or data_first[i] == len(data_first) - 1:
+                data_first_list.append('' . join(data_first[j: i + 1]))
+                j = i
             print(''.join(data_first_list))
-    
-            
-    
-    print (' PRINTING OUT from the second file: \n')
+
+    print(' PRINTING OUT from the second file: \n')
     with open('data_ver2.csv', 'r', encoding='utf-8') as f:
+        data_second = f. readlines()
+        print(data_second)
+
+
+def delete():
+    print(' Please, choose file to proceed with: \n 1 or 2 ')
+    var = int(input())
+    while var != 1 and var != 2:
+        print('incorrect')
+        var = int(input('type in: '))
+    if var == 1:
+        with  open('data_ver1.csv', 'a', encoding='utf-8') as f:
+    elif var == 2:
+        with  open('data_ver2.csv', 'a', encoding='utf-8') as f:
+
+
+def change():
+    print(' Please, choose file to proceed with: \n 1 or 2 ')
+    var = int(input())
+    while var != 1 and var != 2:
+        print('incorrect')
+        var = int(input('type in: '))
+
+    if var == 1:
+        with open('data_ver1.csv', 'a', encoding='utf-8') as f:
+        data_first = f.readlines()
+        data_first_list = []
+        smthtochange = input("Type in what needs to be changed: ")
+        newsmth = input("Type with what it should be replaced: ")
+        # index= smthtochange.find(new)
+        for i in range(len(data_first)):
+            if data_first[i] == smthtochange:
+                data_first[i] = data_first[i].replace(smthtochange, newsmth)
+                print(data_first[i])
+
+    elif var == 2:
+        with open('data_ver2.csv', 'a', encoding='utf-8') as f:
             data_second = f. readlines()
-            print(data_second)
+            smthtochange = input("Type in what needs to be changed: ")
+            newsmth = input("Type with what it should be replaced: ")
+            # index= smthtochange.find(new)
+            for i in range(len(data_first)):
+                if data_first[i] == smthtochange:
+                data_first[i] = data_first[i].replace(smthtochange, newsmth)
+               
 
 
 
 input_data()
 print_data()
+print("Please, what would you like to do next: 1- change 2 - delete 3 - exit")
+v = int(input())
+if v == 1:
+    change()
+elif v == 2:
+    delete()
+elif v == 3:
+    break
+else:
+    print('incorrect')
+    v = int(input('Try again : '))
--- ui.py
+++ ui.py
@@ -1,18 +1,27 @@
-from logger.py import input_data(), print_data()
+from logger import input_data, print_data
 
 
 def interface():
-   print("Добрый день! Вы попали на страницу бота-справочника. \n 1-input \n 2 -choose")
-   command = int(input('type in number: '))
-   
-   while command != 1 and command != 2:
+    print("Добрый день! Вы попали на страницу бота-справочника. \n 1-input \n 2 -choose")
+    command = int(input('type in number: '))
+    while command != 1 and command != 2:
         print("incorrect")
         command = int(input('type in number: '))
-
     if command == 1:
         input_data()
     elif command == 2:
         print_data()
+    print("Что бы вы хотели сделать далее? \n 1- изменить запись 2 - удалить ")
+    choice = int(input('type in number: '))
+    while choice != 1 and choice != 2:
+        print("incorrect")
+        choice = int(input('type in number: '))
+    if choice == 1:
+        
+    elif choice == 2:
+        
+
+interface()
 
 
     
